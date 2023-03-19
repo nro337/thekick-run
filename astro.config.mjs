50a2d2c427d319780c87260daa4366e41d637bd6
nro337 <nro337@gmail.com>
2023-03-19 19:23:38 -0400

    GH Pages Deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  //site: "https://astro-paper.pages.dev/",
+  site: "https://thekick.run",
   integrations: [
     tailwind({
       config: {
